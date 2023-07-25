d4296a11edbcac1a3977e3f4773814c0635eafa6
ProgrammerPeter2 <hp.raszpi@gmail.com>
2023-07-25 15:27:40 +0200

    #3 implemented the OperatorCommandExecutor

---
+++ src/main/java/org/_1104mc/staffstuff/commands/OperatorCommandExecutor.java
@@ -0,0 +1,29 @@
+package org._1104mc.staffstuff.commands;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org._1104mc.staffstuff.operator.OperatorLevel;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class OperatorCommandExecutor implements CommandExecutor {
+    public abstract void execCommand(Player executor, String[] args);
+    public abstract OperatorLevel getRequiredLevel();
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(!(sender instanceof Player)) return false;
+        Player player = (Player) sender;
+        OperatorLevel playerLevel = OperatorLevel.getPlayerLevel(player);
+        if(playerLevel == null || playerLevel.getValue() < getRequiredLevel().getValue()){
+            Component noPermissionMessage = Component.text("You have no permission to execute this command!").color(NamedTextColor.DARK_RED);
+            player.sendMessage(noPermissionMessage);
+            return false;
+        }
+        execCommand(player, args);
+        return true;
+    }
+}
